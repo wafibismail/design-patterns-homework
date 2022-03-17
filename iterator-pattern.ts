8654a668a3bb61ddfa5800634e9dafa633f95d73
wafibismail <56105912+wafibismail@users.noreply.github.com>
2022-03-17 14:42:09 +0800

    Create iterator-pattern.ts

---
+++ iterator-pattern.ts
@@ -0,0 +1,59 @@
+interface Item {
+    label: string;
+}
+
+const WeaponItemTypes = [
+    {label: "Gun", maxUses: 100, maxPower: 3},
+    {label: "Stick", maxUses: 500, maxPower: 5}
+];
+class WeaponItem implements Item {
+    uses: number;
+    power: number;
+    label: string;
+    constructor() {
+        let type = WeaponItemTypes[Math.round(2*Math.random())];
+
+        this.uses = Math.round(Math.max(type.maxUses/5, type.maxUses*Math.random()));
+        this.power = Math.round(type.maxPower*Math.random());
+        this.label = `${type.label} Lvl ${this.power}`;
+    }
+}
+
+interface InventoryIterable {
+    MAX_ITEMS: number;
+    items: Item[];
+    getIterator: () => InventoryIterator;
+}
+
+class WeaponInventoryInterable implements InventoryIterable {
+    MAX_ITEMS = 5;
+    items: WeaponItem[];
+    constructor() {
+        while (this.items.length < this.MAX_ITEMS)
+            this.items.push(new WeaponItem);
+    }
+    getIterator = function() {
+        return new WeaponInventoryIterator(this);
+    };
+}
+
+interface InventoryIterator {
+    inventory: InventoryIterable;
+    index: number;
+    isDone: () => boolean;
+    next: () => void;
+    current: () => Item;
+};
+
+class WeaponInventoryIterator implements InventoryIterator {
+    inventory: WeaponInventoryInterable;
+    index = 0;
+
+    constructor (inventory: WeaponInventoryInterable) {
+        this.inventory = inventory;
+    }
+
+    isDone() {return this.index == this.inventory.items.length};
+    next() {this.index++};
+    current() {return this.inventory[this.index]};
+}
